c7b8dda561a9a6397d12686a66c7b9f1e3da9206
Josh Loecker <joshloecker@icloud.com>
2023-10-16 15:55:48 -0500

    "Updated dependencies and dropped contourpy package"
    In the given diff, several modifications were implemented towards optimizing the dependencies pinned in our poetry.lock file. Specifically, dependencies "exceptiongroup" and "typing_extensions" were added to cater for any python version less than 3.11. On top of that, the version of the "cycler" package was updated to "0.12.1" from "0.12.0" for features or fixes that come with the new version. Modifications were also made to the 'gevent' and 'matplotlib' packages. Lastly, the 'contourpy' package was entirely removed from the dependencies list due to either it being no longer used in our project, or its features being replaced by other packages.

--- async_bioservices/__init__.py
+++ async_bioservices/__init__.py
@@ -1,8 +1,4 @@
-from .async_bioservices import fetch_gene_info
-from .input_database import InputDatabase
-from .output_database import OutputDatabase
-from .taxon_ids import TaxonIDs
-# from . import async_bioservices
-# from . import input_database
-# from . import output_database
-# from . import taxon_ids
+from async_bioservices import async_bioservices
+from async_bioservices import input_database
+from async_bioservices import output_database
+from async_bioservices import taxon_ids
